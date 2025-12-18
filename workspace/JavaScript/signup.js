75bf1f464be32a7e5cbd5c3e7389e5dbeae0181f
msok9559-cyber <msok9559@gmail.com>
2025-12-18 15:47:07 +0900

    Create signup.js

---
+++ workspace/JavaScript/signup.js
@@ -0,0 +1,65 @@
+// 아이디 중복확인 변수
+const userId = document.getElementById("userId");
+const idCheckBtn = document.getElementById("idCheckBtn");
+const idMessage = document.getElementById("idMessage");
+
+// 사용중인 아이디 임의 값
+const usedIds = ["admin", "test", "user123"];
+
+
+//비밀번호 입력 확인 변수
+const pw = document.getElementById("password");
+const pwCheck = document.getElementById("passwordCheck");
+const msg = document.getElementById("pwMessage");
+
+// 체크박스 전체동의시 전체 클릭  변수
+const allClick = document.getElementById("allclick");
+const termItmes = document.querySelectorAll(".trem-item");
+
+
+
+idCheckBtn.addEventListener("click", function () {
+    const inputId = userId.value;
+
+    if (inputId === "") {
+        idMessage.style.display = "none";
+        return;
+    }
+
+    idMessage.style.display = "block";
+
+    if (usedIds.includes(inputId)) {
+        idMessage.textContent = "이미 사용 중인 아이디입니다.";
+        idMessage.style.color = "red";
+    } else {
+        idMessage.textContent = "사용 가능한 아이디입니다.";
+        idMessage.style.color = "green";
+    }
+});
+
+//비밀번호 입력 확인
+pwCheck.addEventListener("keyup", function () {
+    // 둘 중 하나라도 비어 있으면 메시지 숨김
+    if (pw.value === "" || pwCheck.value === "") {
+        msg.style.display = "none";
+        return;
+    }
+
+    // 비밀번호 비교
+    if (pw.value === pwCheck.value) {
+        msg.textContent = "비밀번호가 일치합니다.";
+        msg.style.color = "green";
+        msg.style.display = "block";
+    } else {
+        msg.textContent = "비밀번호가 일치하지 않습니다.";
+        msg.style.color = "red";
+        msg.style.display = "block";
+    }
+});
+
+// 체크박스 전체동의시 전체 클릭 스크립트
+allClick.addEventListener("change", function(){
+    for(let i = 0; i < termItmes.length; i++){
+        termItmes[i].checked = allClick.checked;
+    }
+})
