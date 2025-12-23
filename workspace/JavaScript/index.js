d6b1f5323d475e4c791f3917e231c4845a536fc9
msok9559-cyber <msok9559@gmail.com>
2025-12-23 14:00:42 +0900

    login.js

--- workspace/JavaScript/index.js
+++ workspace/JavaScript/index.js
@@ -1,7 +1,36 @@
 window.addEventListener('load', function () {
-    const search = document.getElementById('search');
 
-    search.addEventListener('click', function () {
-        search.querySelector('a').click();
-    });
+    /* 검색 카드 */
+    const search = document.getElementById('search');
+    if (search) {
+        search.addEventListener('click', function () {
+            search.querySelector('a').click();
+        });
+    }
+
+    /* 로그인 상태 처리 */
+    const loginBtn = document.getElementById("login-btn");
+    const mypageBtn = document.getElementById("mypage-btn");
+
+    const loginUser = localStorage.getItem("loginUser");
+
+    if (loginUser) {
+        loginBtn.textContent = "Log Out";
+        loginBtn.href = "#";
+        mypageBtn.style.display = "inline-block";
+
+        loginBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            localStorage.removeItem("loginUser");
+            alert("로그아웃 되었습니다.");
+            location.reload();
+        });
+
+    } else {
+        loginBtn.textContent = "Log In";
+        loginBtn.href = "./members/login.html";
+        mypageBtn.style.display = "none";
+    }
+
 });
