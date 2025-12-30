d051d5e82cca72a8d0b1d7f6ac64fe8892be3b90
Kim, Jaeung <kimjw5076@naver.com>
2025-12-30 16:16:22 +0900

    수정본

--- docs/JavaScript/index.js
+++ docs/JavaScript/index.js
@@ -1,33 +1,32 @@
 window.addEventListener('load', function () {
-
     /* 검색 카드 */
     const search = document.getElementById('search');
+    const searchLink = document.getElementById('searchLink');
+
+    /* 로그인 상태 처리 */
+    const loginBtn = document.getElementById("login-btn");
+    const mypageBtn = document.getElementById("mypage-btn");
+    const loginUser = localStorage.getItem("loginUser");
+
     if (search) {
         search.addEventListener('click', function () {
-            const loginUser = localStorage.getItem("loginUser");
             if(!loginUser){
                 alert("로그인후 이용가능합니다");
                 location.href = "./members/login.html";
                 return;
             }
 
-            search.querySelector('a').click();
+            searchLink.click();
         });
     }
-
-    /* 로그인 상태 처리 */
-    const loginBtn = document.getElementById("login-btn");
-    const mypageBtn = document.getElementById("mypage-btn");
-
-    const loginUser = localStorage.getItem("loginUser");
 
     if (loginUser) {
         loginBtn.textContent = "Log out";
         loginBtn.href = "#";
         mypageBtn.style.display = "inline-block";
 
-        loginBtn.addEventListener("click", function (e) {
-            e.preventDefault();
+        loginBtn.addEventListener("click", function (event) {
+            event.preventDefault();
 
             localStorage.removeItem("loginUser");
             alert("로그아웃 되었습니다.");
@@ -39,5 +38,4 @@ window.addEventListener('load', function () {
         loginBtn.href = "./members/login.html";
         mypageBtn.style.display = "none";
     }
-
 });
